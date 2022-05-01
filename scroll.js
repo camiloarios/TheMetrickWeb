5ea6de4760153bf9ad8c11709ca845e8ac9bc516
camiloarios <arvicami@gmail.com>
2022-05-01 11:34:14 -0500

    feat: define style and funtionality ofrenovation

--- scroll.js
+++ scroll.js
@@ -22,3 +22,23 @@ globalThis.addEventListener('scroll', function () {
   const isDownHide = newPosition > oldPosition && globalThis.scrollY >= 636;
   header.classList.toggle('header_hide', isDownHide);
 });
+
+const refContainerImg = document.querySelector('.renovations__images');
+const refImg = document.querySelector('.renovations__before');
+const refCircle = document.querySelector('.renovation__circle');
+
+refContainerImg.addEventListener('mousemove', (event) => {
+  const positionX = event.screenX;
+  const windowWidth = globalThis.innerWidth;
+  const containerWidth = refContainerImg.clientWidth;
+
+  const padding = (windowWidth - containerWidth) / 2;
+  const positionLineX = 100 - ((positionX - padding) / containerWidth) * 100;
+  refImg.style.clipPath = `inset(0 ${positionLineX}% 0 0 )`;
+
+  const positionCircle = positionX - padding;
+
+  if (positionCircle > 0 && positionCircle <= containerWidth) {
+    refCircle.style.left = `${positionCircle}px`;
+  }
+});
